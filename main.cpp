99bd50cc58af0abed8d687f279e35b1063d941a8
Mehdi Saleh <94439809+Mehdi-Saleh@users.noreply.github.com>
2025-09-14 16:17:22 +0330

    removed SDL and GLAD

--- main.cpp
+++ main.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
-#include "sdl-example.cpp"
+#include <SDL3/SDL.h>
+#include "opengl-example.cpp"
 
 int main( int argc, char* argv[] )
 {
     Renderer renderer;
     renderer.Init( "Test Window", 800, 600 );
 
-    for ( int i = 0; i < 14400; i++ )
+    std::cout << "Hell-Oh!" << std::endl;
+
+    bool running = true;
+    // while ( running )
+    // {
+    //     SDL_Event event;
+    //     while ( SDL_PollEvent( &event ) ) 
+    //     {
+    //         if ( event.type == SDL_EVENT_QUIT ) 
+    //         {
+    //             running = false;
+    //         }
+    //     }
+        
         renderer.Render();
+    // }
 
     return 0;
 }
