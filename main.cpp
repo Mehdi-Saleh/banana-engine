332611d5a2d5425701f61ac19d4029b20c85b252
Mehdi Saleh <94439809+Mehdi-Saleh@users.noreply.github.com>
2025-09-14 16:37:55 +0330

    Added glfw and glad

--- main.cpp
+++ main.cpp
@@ -1,28 +1,49 @@
 #include <iostream>
-#include <SDL3/SDL.h>
-#include "opengl-example.cpp"
+#include "glad/glad.h"
+#include <GLFW/glfw3.h>
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}  
+
 
 int main( int argc, char* argv[] )
 {
-    Renderer renderer;
-    renderer.Init( "Test Window", 800, 600 );
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    std::cout << "Hell-Oh!" << std::endl;
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    
 
-    bool running = true;
-    // while ( running )
-    // {
-    //     SDL_Event event;
-    //     while ( SDL_PollEvent( &event ) ) 
-    //     {
-    //         if ( event.type == SDL_EVENT_QUIT ) 
-    //         {
-    //             running = false;
-    //         }
-    //     }
-        
-        renderer.Render();
-    // }
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }  
 
+    glViewport(0, 0, 800, 600);
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();    
+    }
+
+    glfwTerminate();
     return 0;
 }
+
