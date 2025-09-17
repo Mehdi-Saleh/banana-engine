8e4795e0176db98a470da2a3169a18b3ccaad781
Mehdi Saleh <94439809+Mehdi-Saleh@users.noreply.github.com>
2025-09-17 13:24:31 +0330

    You can now now switch between tri/rect

--- banana_engine.cpp
+++ banana_engine.cpp
@@ -9,11 +9,17 @@ class BananaEngine
 {
     private: GLFWwindow* window = nullptr;
     private: bool x = false;
+    private: bool z = false;
     private: unsigned int vertexShader;
     private: unsigned int fragmentShader;
+    private: unsigned int fragmentShader2;
     private: unsigned int shaderProgram;
+    private: unsigned int shaderProgram2;
     private: unsigned int triangleVBO;
     private: unsigned int triangleVAO;
+    private: unsigned int rectangleVBO;
+    private: unsigned int rectangleVAO;
+    private: unsigned int rectangleEBO;
 
     private: const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
@@ -29,6 +35,13 @@ class BananaEngine
     "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
     "}\n";
 
+    private: const char *fragmentShaderSource2 = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.9f, 0.2f, 1.0f);\n"
+    "}\n";
+
 
     public: void Start()
     {
@@ -40,6 +53,7 @@ class BananaEngine
 
         LoadShaders();
         LoadTriangle();
+        LoadRectangle();
 
         while( !glfwWindowShouldClose( window ) )
         {
@@ -91,20 +105,20 @@ class BananaEngine
         if ( glfwGetKey( window, GLFW_KEY_ESCAPE ) == GLFW_PRESS )
             glfwSetWindowShouldClose( window, true );
         x = glfwGetKey( window, GLFW_KEY_X ) == GLFW_PRESS;
+        z = glfwGetKey( window, GLFW_KEY_Z ) == GLFW_PRESS;
     }
 
 
     private: void Render()
     {
-        if ( !x )
-            glClearColor( 0.2f, 0.3f, 0.3f, 1.0f );
-        else
-            glClearColor( 0.3f, 0.3f, 0.2f, 1.0f );
+        glClearColor( 0.2f, 0.3f, 0.3f, 1.0f );
         glClear( GL_COLOR_BUFFER_BIT );
-
-        glUseProgram( shaderProgram );
-        glBindVertexArray( triangleVAO );
-        glDrawArrays( GL_TRIANGLES, 0, 3 );
+        
+        // glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
+        if ( !x )
+            DrawRectangle();
+        else
+            DrawTriangle();
     }
 
 
@@ -133,6 +147,16 @@ class BananaEngine
             std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
         }
 
+        fragmentShader2 = glCreateShader( GL_FRAGMENT_SHADER );
+        glShaderSource( fragmentShader2, 1, &fragmentShaderSource2, NULL );
+        glCompileShader( fragmentShader2 );
+        glGetShaderiv( fragmentShader2, GL_COMPILE_STATUS, &success );
+        if ( !success )
+        {
+            glGetShaderInfoLog( fragmentShader2, 512, NULL, infoLog );
+            std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+        }
+
         shaderProgram = glCreateProgram();
         glAttachShader( shaderProgram, vertexShader );
         glAttachShader( shaderProgram, fragmentShader );
@@ -141,6 +165,17 @@ class BananaEngine
         if ( !success )
         {
             glGetProgramInfoLog( shaderProgram, 512, NULL, infoLog );
+            std::cout << "ERROR::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+        }
+
+        shaderProgram2 = glCreateProgram();
+        glAttachShader( shaderProgram2, vertexShader );
+        glAttachShader( shaderProgram2, fragmentShader2 );
+        glLinkProgram( shaderProgram2 );
+        glGetProgramiv( shaderProgram2, GL_LINK_STATUS, &success );
+        if ( !success )
+        {
+            glGetProgramInfoLog( shaderProgram2, 512, NULL, infoLog );
             std::cout << "ERROR::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
         }
 
@@ -156,7 +191,7 @@ class BananaEngine
         float vertices[] = {
             -0.5f, -0.5f, 0.0f,
             0.5f, -0.5f, 0.0f,
-            0.0f,  0.5f, 0.0f
+            0.0f,  0.5f, 0.0f,
         };
         glGenVertexArrays( 1, &triangleVAO );
         glBindVertexArray( triangleVAO );
@@ -165,6 +200,56 @@ class BananaEngine
         glBufferData( GL_ARRAY_BUFFER, sizeof( vertices ), vertices, GL_STATIC_DRAW );
         glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 3*sizeof( float ), (void*)0 );
         glEnableVertexAttribArray( 0 );
+
+        glBindVertexArray( 0 );
+    }
+
+
+    private: void LoadRectangle()
+    {
+        float vertices[] = {
+            -0.5f,  0.5f, 0.0f,
+            0.5f,  0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            -0.5f, -0.5f, 0.0f,
+        };
+        unsigned int indices[] = {
+            0,  1,  2,
+            2,  3,  0,
+        };
+
+        glGenVertexArrays( 1, &rectangleVAO );
+        glBindVertexArray( rectangleVAO );
+        glGenBuffers( 1, &rectangleVBO );
+        glBindBuffer( GL_ARRAY_BUFFER, rectangleVBO );
+        glBufferData( GL_ARRAY_BUFFER, sizeof( vertices ), vertices, GL_STATIC_DRAW );
+
+        glGenBuffers( 1, &rectangleEBO );
+        glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, rectangleEBO );
+        glBufferData( GL_ELEMENT_ARRAY_BUFFER, sizeof( indices ), indices, GL_STATIC_DRAW );
+
+        glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 3*sizeof( float ), (void*)0 );
+        glEnableVertexAttribArray( 0 );
+
+        glBindVertexArray( 0 );
+    }
+
+
+    private: void DrawTriangle()
+    {
+        glUseProgram( z? shaderProgram2 : shaderProgram );
+        glBindVertexArray( triangleVAO );
+        glDrawArrays( GL_TRIANGLES, 0, 3 );
+        glBindVertexArray( 0 );
+    }
+
+
+    private: void DrawRectangle()
+    {
+        glUseProgram( z? shaderProgram2 : shaderProgram );
+        glBindVertexArray( rectangleVAO );
+        glDrawElements( GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0 );
+        glBindVertexArray( 0 );
     }
 
 
